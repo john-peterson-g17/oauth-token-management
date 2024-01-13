78a311103700cc76596cdb0647e11d0ba4d7abb3
John Peterson <peterson.glock17@gmail.com>
2024-01-12 23:13:38 -0700

    Updates for CI fix

--- tests/Helpers/LoadsEnvironmentVariables.php
+++ tests/Helpers/LoadsEnvironmentVariables.php
@@ -7,14 +7,7 @@ use Dotenv\Dotenv;
 trait LoadsEnvironmentVariables {
 
     public function loadEnvironmentVariables() {
-        try {
-            $dotenv = Dotenv::createMutable(__DIR__ . '/../../');
-            $dotenv->load();
-        } catch (\Exception $e) {
-            echo "\nWARNING! Unable to load .env file due to Exception: \n";
-            echo get_class($e);
-            echo "\n" . $e->getMessage() . "\n";
-            echo "Assuming that .env file is not used.\n";
-        }
+        $dotenv = Dotenv::createMutable(__DIR__ . '/../../');
+        $dotenv->safeLoad();
     }
 }
